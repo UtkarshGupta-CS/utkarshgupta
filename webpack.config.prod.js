1b8cf66cd55660e74fc1594970e9d180225ae4ec
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-01-08 03:04:58 +0530

    removed the CompressionPlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,42 +1,30 @@
-var webpack = require('webpack');
-var CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
+	entry: './app/index.js',
+	output: {
 		filename: 'bundle.js',
 		publicPath: '/',
-    path: path.resolve(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-    ],
-  },
-  resolve: {
-    extensions: ['.json', '.js', '.jsx', '.css'],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      //<--key to reduce React's size
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new CompressionPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './public',
-  },
+		path: path.resolve(__dirname, 'public'),
+	},
+	module: {
+		rules: [
+			{ test: /\.js$/, use: 'babel-loader' },
+			{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
+		],
+	},
+	resolve: {
+		extensions: ['.json', '.js', '.jsx', '.css'],
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			//<--key to reduce React's size
+			'process.env': {
+				NODE_ENV: JSON.stringify('production'),
+			},
+		}),
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.AggressiveMergingPlugin(),
+	],
 };
