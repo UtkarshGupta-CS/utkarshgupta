1909d6d4018a085293d126af38536ee9fcb3749f
Utkarsh Gupta <utgupta1997@gmail.com>
2019-08-19 03:26:09 +0530

    Update body

--- app/components/Body.js
+++ app/components/Body.js
@@ -4,9 +4,9 @@ class Body extends Component {
 	render() {
 		return (
 			<div className="content">
-				Howdy,<br /> I'm Utkarsh. I'm an aspiring web developer. Currently pursuing B.Tech from
-				UPES, Dehradun(India). I love building stuff mostly using code. I am not very active on
-				social media but I follow and have discussions with some super amazing people on Twitter.
+				Howdy,<br /> I'm Utkarsh. I'm a Frontend Engineer at <a href="https://atlan.com/"> Atlan </a>.
+				I love building stuff mostly using code.
+				I am not very active on social media but I follow and have discussions with some super amazing people on Twitter.
 				Check out my
 				<a href="https://github.com/UtkarshGupta-CS/"> Github </a>
 				for my side projects and Open Source work. You can also check out my
