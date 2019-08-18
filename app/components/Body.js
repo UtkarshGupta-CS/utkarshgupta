349b86fe5d6a6f244a354f917d7bd5e78d0e7a05
Utkarsh <utgupta1997@gmail.com>
2019-08-19 03:31:50 +0530

    update body

--- app/components/Body.js
+++ app/components/Body.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Body extends Component {
 	render() {
 		return (
 			<div className="content">
-				Howdy,<br /> I'm Utkarsh. I'm a Frontend Engineer at <a href="https://atlan.com/"> Atlan </a>.
-				I love building stuff mostly using code.
-				I am not very active on social media but I follow and have discussions with some super amazing people on Twitter.
-				Check out my
-				<a href="https://github.com/UtkarshGupta-CS/"> Github </a>
+				Howdy,
+				<br /> I'm Utkarsh. I'm a Frontend Engineer at{" "}
+				<a href="https://atlan.com/" target="_blank">
+					{" "}
+					Atlan{" "}
+				</a>
+				. I love building stuff mostly using code. I am not very active on
+				social media but I follow and have discussions with some super amazing
+				people on Twitter. Check out my
+				<a href="https://github.com/UtkarshGupta-CS/" target="_blank">
+					{" "}
+					Github{" "}
+				</a>
 				for my side projects and Open Source work. You can also check out my
-				<a href="https://drive.google.com/open?id=1ifR-vtt1YvO9Yd9G0AjOZ2b7sGztiW7L"> Resume </a>
+				<a
+					href="https://drive.google.com/open?id=1ifR-vtt1YvO9Yd9G0AjOZ2b7sGztiW7L"
+					target="_blank"
+				>
+					{" "}
+					Resume{" "}
+				</a>
 				for more details.
 			</div>
 		);
